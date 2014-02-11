5e519a80dc4219bd5bb3fc80a702d13ae9622c56
Simon Metson <simon+github@cloudant.com>
2014-02-11 12:27:52 +0000

    simplify and get working

--- topGunBot.py
+++ topGunBot.py
@@ -9,48 +9,36 @@ from errbot import BotPlugin, botcmd
 from errbot.utils import get_sender_username
 import config
 import logging
-
+from random import choice
 from topGun import TopGun
 
 
-def generate(character):
-    f = lambda self, mess, args: "(%s) %s" % (character,
-            self.topgun.get_random(character))
-    f.__name__ = character
-    f.__doc__ = "Get a random quote from %s." % character.title()
-    return f
-
-
-class TopGunBotBuilder(type):
-    def __new__(mcs, name, bases, classDict):
-        newClassDict = dict(classDict.items() +
-                            [(character, botcmd(generate(character)))
-                            for character in TopGun.CHARACTERS])
-        return super(TopGunBotBuilder, mcs).__new__(mcs, name, bases,
-                                                    newClassDict)
-
-
 class TopGunBot(BotPlugin):
-    __metaclass__ = TopGunBotBuilder
-    min_err_version = "1.6.0"
+    def activate(self):
+        super(TopGunBot, self).activate()
+        self.topgun_script = TopGun()
+        logging.info(self.topgun_script.CHARACTERS.keys())
 
-
-    def __init__(self):
-        super(BotPlugin, self).__init__()
-        self.topgun = TopGun()
-
+    @botcmd
+    def topgun(self, conn, mess):
+        character = choice(self.topgun_script.CHARACTERS)
+        return "(%s) %s  " % (
+            character, self.topgun_script.get_random(character)
+        )
 
     def callback_message(self, conn, mess):
         """Listen for TOP GUN mentions and interject random lines from those
         characters who were mentioned.
         """
-        if (mess.getFrom().getStripped() == config.BOT_IDENTITY["username"]) or (get_sender_username(mess) == config.CHATROOM_FN):
+        if (mess.getFrom().getStripped() == config.BOT_IDENTITY["nickname"]) \
+                or (get_sender_username(mess) == config.CHATROOM_FN):
             logging.debug("Ignore a message from myself")
             return False
 
-        message = ""
-        for character in TopGun.CHARACTERS:
-            if mess.getBody().find("(%s)" % character) != -1:
-                message += "(%s) %s  " % (character, self.topgun.get_random(character))
-        if message:
-            self.send(mess.getFrom(), message, message_type=mess.getType())
+        for character in self.topgun_script.CHARACTERS:
+            search = "--" + character
+            return search
+            if mess.getBody().startswith(search):
+                return "(%s) %s  " % (
+                    character, self.topgun_script.get_random(character)
+                )
