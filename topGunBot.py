8cc4f75caa83746298707d89bfb05242d19b1ae1
Simon Metson <simon+github@cloudant.com>
2014-02-11 15:26:40 +0000

    make a simpler command

--- topGunBot.py
+++ topGunBot.py
@@ -5,11 +5,9 @@
 mentions and respond with a random line spoken by that character in the film.
 """
 
-from errbot import BotPlugin, botcmd
-from errbot.utils import get_sender_username
-import config
 import logging
 from random import choice
+from errbot import BotPlugin, botcmd
 from topGun import TopGun
 
 
@@ -19,26 +17,11 @@ class TopGunBot(BotPlugin):
         self.topgun_script = TopGun()
         logging.info(self.topgun_script.CHARACTERS.keys())
 
-    @botcmd
-    def topgun(self, conn, mess):
-        character = choice(self.topgun_script.CHARACTERS)
-        return "(%s) %s  " % (
+    @botcmd(split_args_with=' ')
+    def topgun(self, mess, args):
+        character = args[0]
+        if character not in self.topgun_script.CHARACTERS.keys():
+            character = choice(self.topgun_script.CHARACTERS.keys())
+        return '%s: "%s"' % (
             character, self.topgun_script.get_random(character)
         )
-
-    def callback_message(self, conn, mess):
-        """Listen for TOP GUN mentions and interject random lines from those
-        characters who were mentioned.
-        """
-        if (mess.getFrom().getStripped() == config.BOT_IDENTITY["nickname"]) \
-                or (get_sender_username(mess) == config.CHATROOM_FN):
-            logging.debug("Ignore a message from myself")
-            return False
-
-        for character in self.topgun_script.CHARACTERS:
-            search = "--" + character
-            return search
-            if mess.getBody().startswith(search):
-                return "(%s) %s  " % (
-                    character, self.topgun_script.get_random(character)
-                )
